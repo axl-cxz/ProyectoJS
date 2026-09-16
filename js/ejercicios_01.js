6b84dbb9c460d873cf4bb1985d86d0da472c3492
Axl Nuñez <cristianleon20082910@gmail.com>
2026-09-16 10:10:44 -0500

    Subi mi practica de Js

---
+++ js/ejercicios_01.js
@@ -0,0 +1,14 @@
+
+// FASE DE ENTRADA: DECLARACIÓN DE VARIABLES:
+const varones = 255;
+const mujeres = 184;
+
+// FASE DE PROCESO: LA LÓGICA U OPERACIÓN
+const totalPersonas = varones + mujeres; 
+let porcentajeVarones = (varones / totalPersonas) * 100;
+let porcentajeMujeres = (mujeres / totalPersonas) * 100;
+
+// FASE DE SALIDA SE HACE LAS IMPRESIONES EN CONSOLA
+console.log("Total de Colaboradores: ", totalPersonas);
+console.log("Porcentaje de Varones: ", porcentajeVarones.toFixed(2), "%");
+console.log("Porcentaje de Mujeres: ", porcentajeMujeres.toFixed(2), "%");
---
+++ js/ejericio_0.js
@@ -0,0 +1,29 @@
+// Declaración de variables y tipos de datos
+const nombreProducto = "Laptop Gamer";
+let precioOriginal = "4500.99";
+const descuento = 15;
+let stockDisponible = "5";
+let enOferta = "true";
+
+// Calcular el precio con descuento usando operadores
+let precioFinal = precioOriginal - precioOriginal * descuento / 100;
+
+// Mostrar informacion del producto
+console.log("🔷 Producto:", nombreProducto);
+console.log("🔷 Precio Original: S/", precioOriginal);
+console.log("🔷 Descuento:", descuento, "%");
+console.log("🔷 Precio Final: S/", precioFinal.toFixed/(2));
+console.log("🔷 Stock Disponible:", stockDisponible);
+console.log("🔷 En oferta:", enOferta ? "Sí" : "No");
+
+// Simular compra y actualizar stock
+let cantidadComprada = 2;
+
+// Validar si hay stock suficiente
+if (cantidadComprada < stockDisponible) {
+    stockDisponible -= cantidadComprada;
+    console.log("✅ Compra realizada con éxito.");
+    console.log("📦 Stock actualizado:", stockDisponible);
+} else {
+    con0ole.log("❌ No hay suficiente stock disponible.");
+}
