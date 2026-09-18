9b802a7485448710d1307beebd61315eae0bd63c
Axl Nuñez <cristianleon20082910@gmail.com>
2026-09-18 08:02:59 -0500

    Cree el documento 04.js y 05.js

---
+++ js/ejercicio_05.js
@@ -0,0 +1,10 @@
+let usuario = "Angelo";
+const kilometros = 100;
+const gasolinaConsumida = 25;
+
+let combustibleXKm = kilometros / gasolinaConsumida;
+
+console.log("Usuario:", usuario);
+console.log("Kilometros recorridos:", kilometros);
+console.log("Gasolina consumida en el recorrido:", gasolinaConsumida);
+console.log("Gasolina consumita por kilometro:", combustibleXKm);
